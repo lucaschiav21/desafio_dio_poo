b60536c37ff76b512c2240f43ffd06ced3b9e8c8
lucaschiav21 <lucaschiav.silva@gmail.com>
2022-08-13 02:23:13 -0300

    add getters, setters and print.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main (String [] args){
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Intensivão de Java, Teoria & Prática.");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Intensivão de Python, Teoria & Prática, com bônus de Cybersegurança.");
+        curso2.setCargaHoraria(12);
+
+        System.out.println();
+    }
+}
