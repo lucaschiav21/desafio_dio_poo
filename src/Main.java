479ea38ea1378c397349d8db9f4720311968b396
lucaschiav21 <lucaschiav.silva@gmail.com>
2022-08-16 21:15:01 -0300

    adjusting prints.

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,13 @@ public class Main {
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Intensivão de Java, Teoria & Prática.");
         curso1.setCargaHoraria(10);
+        System.out.println(curso1);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso Python");
         curso2.setDescricao("Intensivão de Python, Teoria & Prática, com bônus de Cybersegurança.");
         curso2.setCargaHoraria(12);
 
-        System.out.println();
+        System.out.println(curso2);
     }
 }
